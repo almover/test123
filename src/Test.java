722128e22837f2cfd37c8dd9739987a51282ec2d
lizhou <1094292238@qq.com>
2020-04-12 11:15:14 +0800

    branch test

--- src/Test.java
+++ src/Test.java
@@ -1,4 +1,6 @@
-package PACKAGE_NAME;
-
 public class Test {
+    public static void main(String[] args) {
+        System.out.println("test");
+        System.out.println("create new branch");
+    }
 }
