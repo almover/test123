a33db0e6c5fcec1aee0203159a4a25f659f0cbc6
lizhou <1094292238@qq.com>
2020-04-12 11:23:48 +0800

    test123

--- src/Hello.java
+++ src/Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("afb");
+        System.out.println("afdb");
         System.out.println("123");
     }
 }
