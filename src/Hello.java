566136e21349ea1e9516173c55ce4d758d57b885
lizhou <1094292238@qq.com>
2020-04-12 11:25:26 +0800

    sdff

--- src/Hello.java
+++ src/Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("afb");
+        System.out.println("2351345");
         System.out.println("123");
     }
 }
